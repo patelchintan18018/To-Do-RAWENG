03a07b2953cbbc1b5518c7f8a231cab12c7b6414
Chintan <patelchintan18018@gmail.com>
2025-05-08 16:33:56 +0530

    Project completed

--- backend/index.js
+++ backend/index.js
@@ -27,9 +27,10 @@ app.use('/api/tasks', taskRoutes);
 if(process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get("/*", (req,res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-    })
+    app.get("/*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
+    });
+    
 }
 
 mongoose.connect(process.env.MONGO_URI)

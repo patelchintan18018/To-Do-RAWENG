b015b5fed815129ac21f09f9f9ef46446e346fc8
Chintan <patelchintan18018@gmail.com>
2025-05-08 18:01:17 +0530

    Project completed

--- backend/index.js
+++ backend/index.js
@@ -21,17 +21,21 @@ app.use(cors({
 }))
 app.use(express.json());
 
+console.log('authRoutes is:', authRoutes);
+console.log('taskRoutes is:', taskRoutes);
+
+
 app.use('/api/auth', authRoutes);
 app.use('/api/tasks', taskRoutes);
 
-if(process.env.NODE_ENV === "production"){
+
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
     });
     
-}
+
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
